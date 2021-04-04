781beb7297f7c9da7c2a7abb5b9ab2d8c47f97e1
Beltra <beltra.fabio97@gmail.com>
2021-04-04 23:08:35 +0200

    Added MyRepetition unit test. Refactoring.

--- RepetitionWebApp/src/main/java/com/company/repetitionwebapp/service/RepetitionService.java
+++ RepetitionWebApp/src/main/java/com/company/repetitionwebapp/service/RepetitionService.java
@@ -2,7 +2,6 @@ package com.company.repetitionwebapp.service;
 
 import com.company.repetitionwebapp.domain.*;
 import com.company.repetitionwebapp.repository.*;
-import com.company.repetitionwebapp.security.SecurityUtils;
 import com.company.repetitionwebapp.service.dto.MyRepetitionRS;
 import com.company.repetitionwebapp.service.dto.RepetitionDTO;
 import org.slf4j.Logger;
@@ -13,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Service class for managing users.
@@ -24,26 +22,24 @@ public class RepetitionService {
     private final Logger log = LoggerFactory.getLogger(RepetitionService.class);
 
     private final RepetitionRepository repetitionRepository;
-    private final TutorRepository tutorRepository;
-    private final UserRepository userRepository;
     private final SubjectRepository subjectRepository;
     private final RepetitionStudentRepository repetitionStudentRepository;
+
+    private final TutorService tutorService;
 
     private final CacheManager cacheManager;
 
     public RepetitionService(
         RepetitionRepository repetitionRepository,
-        TutorRepository tutorRepository,
-        UserRepository userRepository,
         SubjectRepository subjectRepository,
         RepetitionStudentRepository repetitionStudentRepository,
+        TutorService tutorService,
         CacheManager cacheManager
     ) {
         this.repetitionRepository = repetitionRepository;
-        this.tutorRepository = tutorRepository;
-        this.userRepository = userRepository;
         this.subjectRepository = subjectRepository;
         this.repetitionStudentRepository = repetitionStudentRepository;
+        this.tutorService = tutorService;
         this.cacheManager = cacheManager;
     }
 
@@ -51,83 +47,57 @@ public class RepetitionService {
 
         List<MyRepetitionRS> myRepetitions = new ArrayList<MyRepetitionRS>();
 
-        SecurityUtils
-            .getCurrentUserLogin()
-            .flatMap(userRepository::findOneByLogin)
-            .ifPresent(
-                user -> {
-                    Tutor tutor = null;
-                    for(Tutor t : tutorRepository.findAll()){
-                        if(t.getUser() != null && t.getUser().getId() == user.getId())
-                            tutor = t;
-                    }
+        Tutor tutor = tutorService.getTutorByUser();
 
-                    if(tutor != null) {
-                        for(Repetition r : repetitionRepository.findAll()){
-                            if(r.getTutor() != null && r.getTutor().getId() == tutor.getId() && r.getDateDeleted() == null)
-                            {
-                                List<Student> students = new ArrayList<Student>();
-                                for(RepetitionStudent s : repetitionStudentRepository.findAll()){
-                                    if(s.getRepetition() != null && s.getRepetition().getId() == r.getId()){
-                                        students.add(s.getStudent());
-                                    }
-                                }
+        if(tutor != null) {
+            repetitionRepository.findAll().stream().filter(r ->
+                r.getTutor() != null && r.getTutor().getId() == tutor.getId() && r.getDateDeleted() == null).forEach(
+                repetition -> {
 
-                                MyRepetitionRS myRepetitionRS = new MyRepetitionRS(r);
-                                myRepetitionRS.setStudents(students);
-                                myRepetitions.add(myRepetitionRS);
-                            }
+                    List<Student> students = new ArrayList<Student>();
+                    for(RepetitionStudent s : repetitionStudentRepository.findAll()){
+                        if(s.getRepetition() != null && s.getRepetition().getId() == repetition.getId() && s.getDateDeleted() == null){
+                            students.add(s.getStudent());
                         }
                     }
+
+                    MyRepetitionRS myRepetitionRS = new MyRepetitionRS(repetition);
+                    myRepetitionRS.setStudents(students);
+                    myRepetitions.add(myRepetitionRS);
                 }
             );
+        }
         return myRepetitions;
     }
 
     public Repetition postRepetition(RepetitionDTO repetitionDTO) {
 
-        AtomicReference<Repetition> result = new AtomicReference<>(new Repetition());
+        Repetition newRepetition = null;
 
-        SecurityUtils
-            .getCurrentUserLogin()
-            .flatMap(userRepository::findOneByLogin)
-            .ifPresent(
-                user -> {
-                    Tutor tutor = null;
-                    List<Tutor> tutors = tutorRepository.findAll();
-                    for(Tutor t : tutors){
-                        if(t.getUser().getId() == user.getId())
-                            tutor = t;
-                    }
+        Tutor tutor = tutorService.getTutorByUser();
+        Subject subject = subjectRepository.findById(repetitionDTO.getSubject().getId()).get();
 
-                    Subject subject = subjectRepository.findById(repetitionDTO.getSubject().getId()).get();
+        if(tutor != null && subject != null) {
 
-                    if(tutor != null && subject != null) {
-
-                        Repetition newRepetition = new Repetition();
-                        newRepetition.setTutor(tutor);
-                        newRepetition.setSubject(subject);
-                        newRepetition.setDuration(repetitionDTO.getDuration());
-                        newRepetition.setDateRepetition(repetitionDTO.getDateRepetition());
-                        newRepetition.setDateCreated(Instant.now());
-                        newRepetition.setDateModified(Instant.now());
-                        newRepetition.setDateDeleted(null);
-                        repetitionRepository.save(newRepetition);
-                        log.debug("Created Information for Repetition: {}", newRepetition);
-
-                        result.set(newRepetition);
-                    }
-                }
-            );
-        return result.get();
+            newRepetition = new Repetition();
+            newRepetition.setTutor(tutor);
+            newRepetition.setSubject(subject);
+            newRepetition.setDuration(repetitionDTO.getDuration());
+            newRepetition.setDateRepetition(repetitionDTO.getDateRepetition());
+            newRepetition.setDateCreated(Instant.now());
+            newRepetition.setDateModified(Instant.now());
+            newRepetition.setDateDeleted(null);
+            repetitionRepository.save(newRepetition);
+            log.debug("Created Information for Repetition: {}", newRepetition);
+        }
+        return newRepetition;
     }
 
     public Repetition updateRepetition(RepetitionDTO repetitionDTO) {
 
-        AtomicReference<Repetition> result = new AtomicReference<>(new Repetition());
+        Repetition repetition = repetitionRepository.findById(repetitionDTO.getId()).get();
 
         Subject subject = subjectRepository.findById(repetitionDTO.getSubject().getId()).get();
-        Repetition repetition = repetitionRepository.findById(repetitionDTO.getId()).get();
 
         if(repetition != null && subject != null) {
 
@@ -137,9 +107,8 @@ public class RepetitionService {
             repetition.setDateModified(Instant.now());
             repetitionRepository.save(repetition);
             log.debug("Updated Information for Repetition: {}", repetition);
-            result.set(repetition);
         }
-        return result.get();
+        return repetition;
     }
 
     public void setDateDeleted(long id) {
---
+++ RepetitionWebApp/src/test/java/com/company/repetitionwebapp/web/rest/MyRepetitionResourceIT.java
@@ -0,0 +1,320 @@
+package com.company.repetitionwebapp.web.rest;
+
+import com.company.repetitionwebapp.RepetitionWebApp;
+import com.company.repetitionwebapp.domain.Repetition;
+import com.company.repetitionwebapp.domain.Subject;
+import com.company.repetitionwebapp.domain.Tutor;
+import com.company.repetitionwebapp.domain.User;
+import com.company.repetitionwebapp.repository.RepetitionRepository;
+import com.company.repetitionwebapp.repository.SubjectRepository;
+import com.company.repetitionwebapp.repository.TutorRepository;
+import com.company.repetitionwebapp.repository.UserRepository;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Integration tests for the {@link RepetitionResource} REST controller.
+ */
+@SpringBootTest(classes = RepetitionWebApp.class)
+@AutoConfigureMockMvc
+@WithMockUser
+public class MyRepetitionResourceIT {
+
+    private static final String DEFAULT_TOPIC = "AAAAAAAAAA";
+    private static final String UPDATED_TOPIC = "BBBBBBBBBB";
+
+    private static final Instant DEFAULT_DATE_REPETITION = Instant.ofEpochMilli(0L);
+    private static final Instant UPDATED_DATE_REPETITION = Instant.now().truncatedTo(ChronoUnit.MILLIS);
+
+    private static final Integer DEFAULT_DURATION = 120;
+    private static final Integer UPDATED_DURATION = 60;
+
+    private static final Instant DEFAULT_DATE_CREATED = Instant.ofEpochMilli(0L);
+    private static final Instant UPDATED_DATE_CREATED = Instant.now().truncatedTo(ChronoUnit.MILLIS);
+
+    private static final Instant DEFAULT_DATE_MODIFIED = Instant.ofEpochMilli(0L);
+    private static final Instant UPDATED_DATE_MODIFIED = Instant.now().truncatedTo(ChronoUnit.MILLIS);
+
+    private static final Instant DEFAULT_DATE_DELETED = null;
+    private static final Instant UPDATED_DATE_DELETED = Instant.now().truncatedTo(ChronoUnit.MILLIS);
+
+    @Autowired
+    private RepetitionRepository repetitionRepository;
+
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TutorRepository tutorRepository;
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private MockMvc restRepetitionMockMvc;
+
+    private Repetition repetition;
+
+    public MyRepetitionResourceIT() {
+    }
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Repetition createEntity(EntityManager em) {
+        Repetition repetition = new Repetition()
+            .dateRepetition(DEFAULT_DATE_REPETITION)
+            .duration(DEFAULT_DURATION)
+            .dateCreated(DEFAULT_DATE_CREATED)
+            .dateModified(DEFAULT_DATE_MODIFIED)
+            .dateDeleted(DEFAULT_DATE_DELETED);
+        return repetition;
+    }
+    /**
+     * Create an updated entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Repetition createUpdatedEntity(EntityManager em) {
+        Repetition repetition = new Repetition()
+            .dateRepetition(UPDATED_DATE_REPETITION)
+            .duration(UPDATED_DURATION)
+            .dateCreated(UPDATED_DATE_CREATED)
+            .dateModified(UPDATED_DATE_MODIFIED)
+            .dateDeleted(UPDATED_DATE_DELETED);
+        return repetition;
+    }
+
+    @BeforeEach
+    public void initTest() {
+        repetition = createEntity(em);
+    }
+
+    @Test
+    @Transactional
+    public void postRepetition() throws Exception {
+        int databaseSizeBeforeCreate = repetitionRepository.findAll().size();
+
+        Optional<User> user = userRepository.findById(Long.valueOf(4));
+
+        Tutor tutor = new Tutor();
+        tutor.setName("TUTOR");
+        tutor.setSurname("TUTOR");
+        tutor.setBirthDate(DEFAULT_DATE_CREATED);
+        tutor.setDateCreated(DEFAULT_DATE_CREATED);
+        tutor.setDateModified(DEFAULT_DATE_MODIFIED);
+        tutor.setUser(user.get());
+        tutorRepository.saveAndFlush(tutor);
+
+        Subject subject = new Subject();
+        subject.setName("Math");
+        subject.setDescription("Math");
+        subject.setImageUrl("");
+
+        subjectRepository.saveAndFlush(subject);
+
+        repetition.setSubject(subject);
+
+        // Create the Repetition
+        restRepetitionMockMvc.perform(post("/api/my-repetitions")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(repetition)))
+            .andExpect(status().isCreated());
+
+        // Validate the Repetition in the database
+        List<Repetition> repetitionList = repetitionRepository.findAll();
+        assertThat(repetitionList).hasSize(databaseSizeBeforeCreate + 1);
+        Repetition testRepetition = repetitionList.get(repetitionList.size() - 1);
+        assertThat(testRepetition.getDuration()).isEqualTo(DEFAULT_DURATION);
+    }
+
+    @Test
+    @Transactional
+    public void createRepetitionWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = repetitionRepository.findAll().size();
+
+        // Create the Repetition with an existing ID
+        repetition.setId(1L);
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restRepetitionMockMvc.perform(post("/api/my-repetitions")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(repetition)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Repetition in the database
+        List<Repetition> repetitionList = repetitionRepository.findAll();
+        assertThat(repetitionList).hasSize(databaseSizeBeforeCreate);
+    }
+
+    @Test
+    @Transactional
+    public void getAllRepetitions() throws Exception {
+        // Initialize the database
+
+        Optional<User> user = userRepository.findById(Long.valueOf(4));
+
+        Tutor tutor = new Tutor();
+        tutor.setName("TUTOR");
+        tutor.setSurname("TUTOR");
+        tutor.setBirthDate(DEFAULT_DATE_CREATED);
+        tutor.setDateCreated(DEFAULT_DATE_CREATED);
+        tutor.setDateModified(DEFAULT_DATE_MODIFIED);
+        tutor.setUser(user.get());
+        tutorRepository.saveAndFlush(tutor);
+
+        Subject subject = new Subject();
+        subject.setName("Math");
+        subject.setDescription("Math");
+        subject.setImageUrl("");
+        subjectRepository.saveAndFlush(subject);
+
+        repetition.setTutor(tutor);
+        repetition.setSubject(subject);
+
+        repetitionRepository.saveAndFlush(repetition);
+
+        // Get all the repetitionList
+        restRepetitionMockMvc.perform(get("/api/my-repetitions"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(repetition.getId().intValue())))
+            .andExpect(jsonPath("$.[*].dateRepetition").value(hasItem(DEFAULT_DATE_REPETITION.toString())))
+            .andExpect(jsonPath("$.[*].duration").value(hasItem(DEFAULT_DURATION)));
+    }
+
+    @Test
+    @Transactional
+    public void getRepetition() throws Exception {
+        // Initialize the database
+        repetitionRepository.saveAndFlush(repetition);
+
+        // Get the repetition
+        restRepetitionMockMvc.perform(get("/api/my-repetitions/{id}", repetition.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+            .andExpect(jsonPath("$.id").value(repetition.getId().intValue()))
+            .andExpect(jsonPath("$.dateRepetition").value(DEFAULT_DATE_REPETITION.toString()))
+            .andExpect(jsonPath("$.duration").value(DEFAULT_DURATION));
+    }
+    @Test
+    @Transactional
+    public void getNonExistingRepetition() throws Exception {
+        // Get the repetition
+        restRepetitionMockMvc.perform(get("/api/my-repetitions/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @Transactional
+    public void updateRepetition() throws Exception {
+        // Initialize the database
+        Optional<User> user = userRepository.findById(Long.valueOf(4));
+
+        Tutor tutor = new Tutor();
+        tutor.setName("TUTOR");
+        tutor.setSurname("TUTOR");
+        tutor.setBirthDate(DEFAULT_DATE_CREATED);
+        tutor.setDateCreated(DEFAULT_DATE_CREATED);
+        tutor.setDateModified(DEFAULT_DATE_MODIFIED);
+        tutor.setUser(user.get());
+        tutorRepository.saveAndFlush(tutor);
+
+        Subject subject = new Subject();
+        subject.setName("Math");
+        subject.setDescription("Math");
+        subject.setImageUrl("");
+        subjectRepository.saveAndFlush(subject);
+
+        repetition.setTutor(tutor);
+        repetition.setSubject(subject);
+
+        repetitionRepository.saveAndFlush(repetition);
+
+        int databaseSizeBeforeUpdate = repetitionRepository.findAll().size();
+
+        // Update the repetition
+        Repetition updatedRepetition = repetitionRepository.findById(repetition.getId()).get();
+        // Disconnect from session so that the updates on updatedRepetition are not directly saved in db
+        em.detach(updatedRepetition);
+        updatedRepetition
+            .topic(UPDATED_TOPIC)
+            .dateRepetition(UPDATED_DATE_REPETITION)
+            .duration(UPDATED_DURATION)
+            .dateCreated(UPDATED_DATE_CREATED)
+            .dateModified(UPDATED_DATE_MODIFIED)
+            .dateDeleted(UPDATED_DATE_DELETED);
+
+        restRepetitionMockMvc.perform(put("/api/my-repetitions")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(updatedRepetition)))
+            .andExpect(status().isOk());
+
+        // Validate the Repetition in the database
+        List<Repetition> repetitionList = repetitionRepository.findAll();
+        assertThat(repetitionList).hasSize(databaseSizeBeforeUpdate);
+        Repetition testRepetition = repetitionList.get(repetitionList.size() - 1);
+        assertThat(testRepetition.getDateRepetition()).isEqualTo(UPDATED_DATE_REPETITION);
+        assertThat(testRepetition.getDuration()).isEqualTo(UPDATED_DURATION);
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistingRepetition() throws Exception {
+        int databaseSizeBeforeUpdate = repetitionRepository.findAll().size();
+
+        // If the entity doesn't have an ID, it will throw BadRequestAlertException
+        restRepetitionMockMvc.perform(put("/api/my-repetitions")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtil.convertObjectToJsonBytes(repetition)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Repetition in the database
+        List<Repetition> repetitionList = repetitionRepository.findAll();
+        assertThat(repetitionList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    public void deleteRepetition() throws Exception {
+        // Initialize the database
+        repetitionRepository.saveAndFlush(repetition);
+
+        int databaseSizeBeforeDelete = repetitionRepository.findAll().size();
+
+        // Delete the repetition
+        restRepetitionMockMvc.perform(delete("/api/my-repetitions/{id}", repetition.getId())
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNoContent());
+
+        // Validate the database contains one less item
+        List<Repetition> repetitionList = repetitionRepository.findAll();
+        assertThat(repetitionList).hasSize(databaseSizeBeforeDelete);
+    }
+}
